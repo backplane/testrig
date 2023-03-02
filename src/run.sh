d91bf187385e78b096a030aa3b3106131492ae38
actualben <actualben@users.noreply.github.com>
2023-03-02 14:16:43 +0100

    update various deps; shellcheck-fixes

--- src/run.sh
+++ src/run.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 # activate venv, start the testrig
+# shellcheck disable=SC1091
 
-VENV="$(dirname "$0")/venv"
-TESTRIG="$(dirname "$0")/testrig.py"
+VENV=$(dirname "$0")/venv
+TESTRIG=$(dirname "$0")/testrig.py
 
 . "${VENV}/bin/activate"
 exec "$TESTRIG" "$@"
